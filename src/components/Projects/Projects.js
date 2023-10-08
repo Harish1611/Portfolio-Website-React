98680564c325c0500f6e6a4cffe6cff4434d81fa
Harish1611 <harishnaidub005@gmail.com>
2023-10-08 23:39:21 +0530

    Project Component: Basic Structure Added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import HeadingDivider from "../UI-Elements/HeadingDivider";
 
 const Projects = () => {
   return (
     <Container fluid className="section-style">
-      <h1> This is Project Page</h1>
+      <h2 className="heading-mystory"> My Recent Projects </h2>
+      <HeadingDivider color={"white"} />
+      <Row></Row>
+      <Row>
+        <p className="mystory" style={{ paddingBottom: "200px" }}>
+          {" "}
+          All the projects will be display here in the card format
+        </p>
+      </Row>
     </Container>
   );
 };
