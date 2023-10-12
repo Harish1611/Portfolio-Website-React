8681355adf0f093d8ef0ae5357d7b5783e768822
Harish1611 <harishnaidub005@gmail.com>
2023-10-12 14:14:23 +0530

    Created Project Component and Reusable ProjectCard Component

---
+++ src/components/Projects/ProjectCard.js
@@ -0,0 +1,33 @@
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { CgWebsite } from "react-icons/cg";
+import { BsGithub } from "react-icons/bs";
+
+function ProjectCard(props) {
+  return (
+    <Card className="project-card-view">
+      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Text style={{ textAlign: "justify" }}>
+          {props.description}
+        </Card.Text>
+        <Button variant="primary" href={props.ghLink} target="_blank">
+          <BsGithub /> &nbsp; GitHub
+        </Button>
+        {"\n"}
+        {"\n"}
+        <Button
+          variant="primary"
+          href={props.demoLink}
+          target="_blank"
+          style={{ marginLeft: "10px" }}
+        >
+          <CgWebsite /> &nbsp;
+          {"Demo"}
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+export default ProjectCard;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,19 +1,84 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import HeadingDivider from "../UI-Elements/HeadingDivider";
+import ProjectCard from "./ProjectCard";
+const placeHolder = "https://placehold.co/600x400";
 
 const Projects = () => {
   return (
     <Container fluid className="section-style">
       <h2 className="heading-mystory"> My Recent Projects </h2>
       <HeadingDivider color={"white"} />
-      <Row></Row>
-      <Row>
-        <p className="mystory" style={{ paddingBottom: "200px" }}>
-          {" "}
-          All the projects will be display here in the card format
-        </p>
-      </Row>
+
+      <Container>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={placeHolder}
+              isBlog={false}
+              title="Big Sale - eCommerce Website"
+              description=""
+              ghLink=""
+              demoLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={placeHolder}
+              isBlog={false}
+              title="My Protfolio - React"
+              description=""
+              ghLink=""
+              demoLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={placeHolder}
+              isBlog={false}
+              title="Drum Game"
+              description=""
+              ghLink=""
+              demoLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={placeHolder}
+              isBlog={false}
+              title="IoT Disinfection Booth"
+              description=""
+              ghLink=""
+              demoLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={placeHolder}
+              isBlog={false}
+              title="RSA & LSB Encryption App"
+              description=""
+              ghLink=""
+              demoLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={placeHolder}
+              isBlog={false}
+              title="Place Holder"
+              description=""
+              ghLink=""
+              demoLink=""
+            />
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 };
