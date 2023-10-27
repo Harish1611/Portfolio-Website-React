17152ea1e70342812bea89f61972d5fab728a369
Harish1611 <harishnaidub005@gmail.com>
2023-10-27 21:30:23 +0530

    New: In view Animation Added to the Hero image & content using react-spring

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,24 +2,53 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import laptopImg from "../../Assets/about.png";
 import HomeSection2 from "./HomeSection2";
+import { useSpring, animated, config, useInView } from "react-spring";
 
 const Home = () => {
+  const [heroRef, heroInView] = useInView({
+    triggerOnce: true,
+  });
+  const heroAnimation = useSpring({
+    opacity: heroInView ? 1 : 0,
+    transform: heroInView ? "translate3d(0, 0, 0)" : "translate3d(90%, 0, 0)",
+    config: config.default,
+  });
+
+  const [heroContentRef, heroContentInView] = useInView({
+    triggerOnce: true,
+  });
+  const heroContent = useSpring({
+    opacity: heroContentInView ? 1 : 0,
+    transform: heroContentInView
+      ? "translate3d(0, 0, 0)"
+      : "translate3d(-90%, 0, 0)",
+
+    config: config.default,
+  });
   return (
     <section>
       <Container fluid className="home-section" id="home">
         <Container className="home-content">
           <Row>
             <Col md={6} className="home-header">
-              <img src={laptopImg} alt="about" className="img-fluid" />
+              <animated.div
+                className="hero"
+                ref={heroRef}
+                style={heroAnimation}
+              >
+                <img src={laptopImg} alt="about" className="img-fluid" />
+              </animated.div>
             </Col>
 
             <Col md={5} style={{ paddingBottom: 20 }}>
-              <h1 className="heading-name headingstyle">
-                I'm
-                <strong className="main-name"> Harish Naidu</strong>
-              </h1>
-              <h2 className="heading-namesub">A Web Developer</h2>
-              <div style={{ padding: 50, textAlign: "left" }}></div>
+              <animated.div ref={heroContentRef} style={heroContent}>
+                <h1 className="heading-name headingstyle">
+                  I'm
+                  <strong className="main-name"> Harish Naidu</strong>
+                </h1>
+                <h2 className="heading-namesub">A Web Developer</h2>
+                <div style={{ padding: 50, textAlign: "left" }}></div>
+              </animated.div>
             </Col>
           </Row>
         </Container>
