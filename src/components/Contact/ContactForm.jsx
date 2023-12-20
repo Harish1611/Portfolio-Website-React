2ecb93751b8c1a1beca7b943f79c950c120c8d8b
Harish1611 <harishnaidub005@gmail.com>
2023-12-20 23:36:31 +0530

    New: Confirmation Toast added for contact form using react-toastify

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,70 +1,78 @@
 import React, { Component } from "react";
 import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 import ConfirmationPopup from "../UI-Elements/ConfirmationPopup";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
+const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+const USER_ID = process.env.REACT_APP_USER_ID;
 
-const SERVICE_ID=process.env.REACT_APP_SERVICE_ID;
-const TEMPLATE_ID=process.env.REACT_APP_TEMPLATE_ID;
-const USER_ID=process.env.REACT_APP_USER_ID;
-
-
-
-
-const ContactForm = () =>  {
-
+const ContactForm = () => {
   const [formData, setFormData] = useState({
-    from_name: '',
-    from_email: '',
-    from_phone: '',
-    message:''
+    from_name: "",
+    from_email: "",
+    from_phone: "",
+    message: "",
   });
-
-  const [showConfirmation, setShowConfirmation] = useState(false);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const toastifyPopup = (response) => {
+    if (response.status === 200) {
+      toast("Message Sent");
+    } else {
+      toast("something went wrong!");
+    }
+  };
+
   const contactFormHandler = (e) => {
     e.preventDefault();
 
-    emailjs.send(
-      SERVICE_ID,
-      TEMPLATE_ID,
-      formData,
-      USER_ID
-    )
+    emailjs
+      .send(SERVICE_ID, TEMPLATE_ID, formData, USER_ID)
       .then((response) => {
-        console.log('Email sent successfully:', response);
-        setShowConfirmation(true);
-        emailjs.send(
-          'service_r7fxnnj',
-          'template_47751oi',
-          { from_email: formData.from_email,from_name: formData.from_name, subject: 'Confirmation',  message: 'Thank you for contacting us! We have received your message. We\'ll get in touch with you soon.' 
-        },
-          '6bhdj5Qga9uIEwIaY'
-        )
+        console.log("Email sent successfully:", response);
+        toastifyPopup(response);
+
+        emailjs
+          .send(
+            "service_r7fxnnj",
+            "template_47751oi",
+            {
+              from_email: formData.from_email,
+              from_name: formData.from_name,
+              subject: "Confirmation",
+              message:
+                "Thank you for contacting us! We have received your message. We'll get in touch with you soon.",
+            },
+            "6bhdj5Qga9uIEwIaY"
+          )
           .then((clientResponse) => {
-            console.log('Client confirmation email sent successfully:', clientResponse);
+            console.log(
+              "Client confirmation email sent successfully:",
+              clientResponse
+            );
           })
           .catch((clientError) => {
-            console.error('Client confirmation email sending failed:', clientError);
+            console.error(
+              "Client confirmation email sending failed:",
+              clientError
+            );
           });
       })
       .catch((error) => {
-        console.error('Email sending failed:', error);
+        console.error("Email sending failed:", error);
+        toastifyPopup(error);
       });
   };
 
-  const closeConfirmation = () => {
-    setShowConfirmation(false);
-  };
-
-
-    return (
-      <div>
+  return (
+    <div>
       <Form onSubmit={contactFormHandler}>
         <Form.Group controlId="name" style={{ paddingTop: "20px" }}>
           <Form.Control
@@ -108,16 +116,28 @@ const ContactForm = () =>  {
         </Button>
       </Form>
 
-       {/* ConfirmationPopup component */}
-       {showConfirmation && (
+      {/* ConfirmationPopup component */}
+      {/* {showConfirmation && (
         <ConfirmationPopup
           message="Thank you for Submission!"
           onClose={closeConfirmation}
         />
-      )}
-      </div>
-    );
-  }
+      )} */}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
 
+      />
+    </div>
+  );
+};
 
 export default ContactForm;
